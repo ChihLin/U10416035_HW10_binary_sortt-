24510efa9cf35e29765fcc6ae60a92aa92acfe9c
ChihLin <azs128821@gmail.com>
2016-05-15 00:17:59 +0800

    create a test class to run  the program
    use array to put number,
    and new an object to use the class,
    and invoke it to arrange the number in the array.

---
+++ TestBinaryTree.java
@@ -0,0 +1,15 @@
+//U10416035 Cheng Chih Lin 
+
+public class TestBinaryTree{
+
+	public static void main(String[] args){
+	
+		int[]  arr = {2,8,65,45,15,4,77,58,42,53,72,25,74,52};
+	
+		BinaryTree bt = new BinaryTree(arr);
+		
+		bt.inorder();
+		
+	}
+
+}
