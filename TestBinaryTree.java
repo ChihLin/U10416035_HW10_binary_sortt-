672d3de72b238b618b1a12762d7f6691cd4652ff
ChihLin <azs128821@gmail.com>
2016-05-16 00:39:42 +0800

    add the comment
    add the comment and add the whole sentence (print it precisely)

--- TestBinaryTree.java
+++ TestBinaryTree.java
@@ -4,9 +4,21 @@ public class TestBinaryTree{
 
 	public static void main(String[] args){
 	
+		//create an array
 		int[]  arr = {2,8,65,45,15,4,77,58,42,53,72,25,74,52};
 	
+		//new an object
 		BinaryTree bt = new BinaryTree(arr);
+		
+		//print the result
+		System.out.println("The array 's number :");
+		for(int i = 0 ; i< arr.length ; i++){
+			
+			System.out.print(arr[i] +"  ");
+		}
+		
+		System.out.println(" ");
+		System.out.println("After binary tree 's numebr : ");
 		
 		bt.inorder();
 		
